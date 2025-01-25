8f1651a49b582532ec6a6f91ae4eef941787d98b
kumar khatri <166585190+Kumarkhatri2003@users.noreply.github.com>
2025-01-25 22:04:05 +0545

    Update sql_con.py

--- sql_con.py
+++ sql_con.py
@@ -4,7 +4,7 @@ def get_connection():
     conn = mysql.connector.connect(
         host ="localhost",
         user="root",
-        password="Kumar@#$123",
+        password="Your_password",
         database = "electricity_billing"
     )
     return conn
